e346968f4ad04580d3e8ed2de57e9d8f99af8645
Wataru Yahiro <wataru@mac-book-pro.local>
2019-05-08 22:44:57 +0900

    fix typo

--- switch_prompt.fish
+++ switch_prompt.fish
@@ -1,6 +1,6 @@
 function switch_prompt -a mode
     set -g PROMPT_MODE $mode
-    if test -z "$PROMPT_MODE" -o "0" -eq 0
+    if test -z "$PROMPT_MODE" -o "$PROMPT_MODE" -eq 0
         set -g PROMPT_MODE 'default'
     end
     echo "Prompt switched to $PROMPT_MODE."
